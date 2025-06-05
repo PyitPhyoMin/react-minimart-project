0686ad756ae67077b40678e5e5a9ff538351cc86
PyitPhyoMin <pyitphyo.min@gmail.com>
2025-06-06 00:00:17 +0800

    changed api

--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const MOCK_API_BASE_URL = "https://677be8e220824100c07b3800.mockapi.io/";
+// Changed the mock API base URL to a different endpoint[Min, 20250605]
+const MOCK_API_BASE_URL = "https://6783cbfe8b6c7a1316f59f54.mockapi.io/api/";
+//const MOCK_API_BASE_URL = "https://677be8e220824100c07b3800.mockapi.io/";
 //Testing using with cors-anywhere.[Min]
 /* const MOCK_API_BASE_URL = "https://cors-anywhere.herokuapp.com/https://677be8e220824100c07b3800.mockapi.io/"; */
 const mockApi = axios.create({
